41c873303ce933a2cfb2f174bbbc90b9851c9360
Jim Cowart <ashbylane@gmail.com>
2011-08-08 02:55:00 -0400

    Additions to the customer UI View

--- ShortOrderExample/client/js-app/main.js
+++ ShortOrderExample/client/js-app/main.js
@@ -1,26 +1,86 @@
 // binds to the order namespace
-/*var orderViewModel = {
-    name: ko.observable(""),
-    orderNumber: ko.observable(""),
+var orderViewModel = {
+    isVisible: ko.observable(true),
+    name: ko.observable("Jimbo"),
+    orderNumber: ko.observable("123456789"),
     items: ko.observableArray([])
 };
 
-var orderHistoryViewModel = {
+orderViewModel.cost = ko.dependentObservable(function() {
+    var cost = 0;
+    this.items().forEach(function(item) {
+        cost += (item.qty() * item.pricePerUnit()) || -1;
+    });
+    return "$ " + (cost).toFixed(2);
+}, orderViewModel);
+
+postal.subscribe("order.items.remove", function(item) {
+    var target = orderViewModel.items().filter(function(x) { return x.id() === item.id; });
+    if(target.length !== 0) {
+        orderViewModel.items.remove(target[0]);
+    }
+});
+
+postal.subscribe("order.items.add", function(item) {
+    var target = orderViewModel.items().filter(function(x) { return x.id() === item.id; });
+    if(target.length === 0) {
+        item.qty = 1;
+        var orderedItem = ko.mapping.fromJS(item);
+        orderedItem.dispQty = ko.dependentObservable(function() {
+            if(this.qty() < 10) {
+                return "0" + this.qty().toString(10);
+            }
+            return this.qty();
+        }, orderedItem);
+        orderedItem.qtyClass = ko.dependentObservable(function() {
+            var cls = "";
+            switch(this.qty()){
+                case 1:
+                    cls = "lightQty";
+                    break;
+                case 2:
+                    cls = "dimQty";
+                    break;
+                case 3:
+                    cls = "darkQty";
+                    break;
+                default:
+                    cls = "darkestQty";
+                    break;
+            };
+            return cls;
+        }, orderedItem);
+        orderedItem.remove = function() {
+            postal.publish("order.items.remove", ko.mapping.toJS(orderedItem));
+        };
+        orderViewModel.items.push(orderedItem);
+    }
+    else {
+        target[0].qty(target[0].qty() + 1);
+    }
+    //ko.applyBindings(orderViewModel, 'order');
+});
+
+var menuItemPrototype = {
+    addMenuItem: function() {
+        postal.publish("order.items.add", this);
+    }
+};
+
+postal.subscribe("menu.get", function(data) {
+    var menuViewModel = ko.mapping.fromJS(data);
+    var len = menuViewModel.items().length;
+    menuViewModel.items().forEach(function(item) {
+        item.addMenuItem = function() {
+            postal.publish("order.items.add", ko.mapping.toJS(item));
+        }
+    });
+    ko.applyBindings(menuViewModel, 'menu');
+});
+
+/*var orderHistoryViewModel = {
     orders: ko.observableArray([])
 };*/
-
-// binds to the menu namespace;
-var menuViewModel = ({
-    items: ko.observableArray([]),
-
-    init: function() {
-        postal.subscribe("menu.get", function(data) {
-            this.items.removeAll();
-            this.items(data.items);
-        }.bind(this));
-        return this;
-    }
-}).init();
 
 /*var infoViewModel = {
     messages: ko.observableArray([])
@@ -37,9 +97,8 @@ $(function() {
         templateSuffix: ".html"
     });
 
-    //ko.applyBindings(orderViewModel, 'order');
+    ko.applyBindings(orderViewModel, 'order');
     //ko.applyBindings(orderHistoryViewModel, 'history');
-    ko.applyBindings(menuViewModel);
     //ko.applyBindings(infoViewModel, 'info');
 
     repository.getMenu();
--- ShortOrderExample/client/js-app/request.js
+++ ShortOrderExample/client/js-app/request.js
@@ -6,10 +6,10 @@
     amplify.request.define(FETCH_MENU, function(settings){
         var fakeMenu = {
             items: [
-                { description: "Burger", pricePerUnit: 1.00 },
-                { description: "Fries", pricePerUnit: .50 },
-                { description: "Drink", pricePerUnit: 1.25 },
-                { description: "Milkshake", pricePerUnit: 2.00 },
+                { id: 0, description: "Burger",    pricePerUnit: (1.00).toFixed(2), imageUrl: "/img/burger.png" },
+                { id: 1, description: "Fries",     pricePerUnit: (.50).toFixed(2),  imageUrl: "/img/fries.png" },
+                { id: 2, description: "Drink",     pricePerUnit: (1.25).toFixed(2), imageUrl: "/img/drink.png" },
+                { id: 3, description: "Milkshake", pricePerUnit: (2.00).toFixed(2), imageUrl: "/img/shake.png" },
             ]
         };
         settings.success(fakeMenu);
